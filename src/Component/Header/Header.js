3792f31eec0459b60b4519c087f3e75701a0df58
Kukuh Rahmadani <krahmadani1@gmail.com>
2022-02-06 18:36:55 +0700

    Missing Image

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     return <div className="header-area" id="headerArea">
   <div className="container">
     <div className="header-content header-style-five position-relative d-flex align-items-center justify-content-between">
-      <div className="logo-wrapper"><a href="page-home.html"><img src="/template/img/core-img/logo.png" alt="" /></a></div>
+      <div className="logo-wrapper"><a href="page-home.html"><img src="/template/img/bg-img/2.jpg" alt="" /></a></div>
       <div className="navbar--toggler" id="affanNavbarToggler" data-bs-toggle="offcanvas" data-bs-target="#affanOffcanvas" aria-controls="affanOffcanvas"><span className="d-block" /><span className="d-block" /><span className="d-block" /></div>
     </div>
   </div>
